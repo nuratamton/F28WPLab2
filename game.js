8b9128381f269237821c3a9fc2352c67943dcfba
Nura Tamton <nrt2000@hw.ac.uk>
2021-11-02 10:40:24 +0400

    final commit

--- game.js
+++ game.js
@@ -116,7 +116,7 @@ function createBeeImg(wNum) {
   let boardDivY = boardDiv.offsetTop;
   //create the IMG element
   let img = document.createElement("img");
-  img.setAttribute("src", "images/bee.gif");
+  img.setAttribute("src", "images/bee.gif"); //setting the  atrributes
   img.setAttribute("width", "100");
   img.setAttribute("alt", "A bee!");
   img.setAttribute("id", "bee" + wNum);
